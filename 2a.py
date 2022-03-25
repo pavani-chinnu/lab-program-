5d149a8064b236fadd790d513118415929c0e667
pavani-chinnu <102370189+pavani-chinnu@users.noreply.github.com>
2022-03-25 21:02:02 +0530

    Add files via upload

---
+++ 2a.py
@@ -0,0 +1,8 @@
+#to print the sum and average of three numbers
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+num3=int(input("enter third number"))
+totalsum=num1+num2+num3
+totalavg=totalsum/num3
+print("sum of three numbers",totalsum)
+print("avg of three numbers",totalavg)
